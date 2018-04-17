c093689b6533f259c32bca172edb77b8fa21bdc2
Fran <fran@aspgems.com>
2018-04-17 19:28:53 +0200

    Create Server class

---
+++ src/threads/Server.java
@@ -0,0 +1,18 @@
+package threads;
+
+public class Server {
+	//Levanto las 3 instancias de los servidores dependiendo de los parametros que me pase el "main-cliente-script"
+	
+	private Process process[]; //Será un vector de 2 elementos, ya que cada servidor tendrá 2 procesos
+	
+	public Server(){
+		
+	}
+	
+	/**
+	 * Sincronizar comienzo de todos los procesos al iniciar
+	 */
+	public void synch() {
+		
+	}
+}
