2e5972c8094f1eeae9985869362767be064baa4d
Fran <fran@aspgems.com>
2018-04-17 17:36:19 +0200

    Begin process schema with loop

--- src/threads/Process.java
+++ src/threads/Process.java
@@ -5,9 +5,12 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Process extends Thread{
 	
 	private String id;
+	///////private Tail tail;
+	private int order = 0;
+	private double timestamp;
 	
 	public Process() {
-		
+		///////tail = new Tail();
 	}
 	
 	@Override
@@ -15,10 +18,11 @@ public class Process extends Thread{
 		//Repetir 100 veces
 		for(int i = 1; i<=100; i++) {
 			//Crear un identificador único de mensaje mediante el numero de mensaje y el identificador de proceso
-			String messageId = "P" + id + " " + i; //<P(id) i>
-			//Message message = new Message(messageId,,,,,);
+			String idMessage = "P" + id + " " + i; //<P(id) i>
+			////////Message message = new Message(idMessage, content, state, order, proposedOrder);
 			
 			//Multidifundir el mensaje ‘Pxx nnn’ donde xx es el identificador de proceso y nnn el número de mensaje
+			
 			
 			//Dormir un tiempo aleatorio entre 1.0 y 1.5 seg
 			double rand = ThreadLocalRandom.current().nextDouble(1.0, 1.5);
@@ -30,15 +34,15 @@ public class Process extends Thread{
 		}
 	}
 	
-	public void sendMessage() {
+	/*public void receiveMessage(Message message, String idMessage) {
 		
 	}
 	
-	public void sendProposed() {
+	public void receiveProposed(Message message) {
 		
 	}
 	
-	public void sendAgreed() {
+	public void receiveAgreed(Message message) {
 		
-	}
+	}*/
 }
