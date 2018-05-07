9f39649bb797c65b20e03cbaa894264f68fd69f2
Fran <danero1511@gmail.com>
2018-05-07 19:28:36 +0200

    Add web config file and modify REST

--- src/controllers/Server.java
+++ src/controllers/Server.java
@@ -1,19 +1,20 @@
 package controllers;
 
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+
+@Singleton
+@Path("server")
 public class Server {
-	//Levanto las 3 instancias de los servidores dependiendo de los parametros que me pase el "main-cliente-script"
-	
 	private static int ready = 0;//numero de procesos preparados
 	private Process process[]; //Será un vector de 2 elementos, ya que cada servidor tendrá 2 procesos
 	private int ip; //ID del servidor (de momento no es una IPv4
 	
-	public Server(int ip){
-		this.ip = ip;
-	}
-	
 	/**
 	 * Sincronizar comienzo de todos los procesos al iniciar
 	 */
+	@GET
+	@Path("synch")
 	public void synch() {
 		//Esto será llamado por cada process antes del bucle
 		synchronized(getClass()) {
@@ -35,9 +36,18 @@ public class Server {
 		
 	}
 	
+	@GET
+	@Path("create")
+	public void createInstance(@DefaultValue("null") @QueryParam(value = "params") String params) {
+		//params es lo que recibe del cliente directamente.
+		//Los parametros seran: ?????????
+	}
+	
 	/**
 	 * Selecciona el proceso al que le llegará el mensaje multicast
 	 */
+	@GET
+	@Path("multicast")
 	public void dispatchMulticastMessage() {
 		//TODO recoger mensaje del multicast
 		//TODO elegir el servidor dependiendo del proceso al que se envie y enviar multicast
@@ -47,6 +57,8 @@ public class Server {
 	/**
 	 * Selecciona el proceso al que le llegará el mensaje de propuesta
 	 */
+	@GET
+	@Path("propose")
 	public void dispatchProposed() {
 		//TODO recibir mensaje p2p
 		//TODO elegir el servidor dependiendo del proceso al que se envie y enviar propuesta
@@ -55,6 +67,8 @@ public class Server {
 	/**
 	 * Selecciona el proceso al que le llegará el mensaje de acuerdo
 	 */
+	@GET
+	@Path("agree")
 	public void dispatchAgreed() {
 		//TODO recibir mensaje p2p
 		//TODO elegir el servidor dependiendo del proceso al que se envie y enviar acuerdo
